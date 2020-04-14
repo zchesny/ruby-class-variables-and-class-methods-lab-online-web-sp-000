b5d25014c9eaeb57ce0c2ed04acb6854eb7149f5
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 16:56:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = {}
+  @@artists = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    #@@genres.has_key?(genre) ? @@genres[genre] += 1 : @@genres[genre] = 1
+    binding.pry
+    @@artists[artist] = 1
+    # if @@artists.include?(artist)
+    #   @@artists[artist] = @@artists[artist] + 1
+    # else
+    #   @@artists[artist] = 1
+    # end
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.keys
+  end
+
+  def self.artists
+    @@artists.keys
+  end
+
+  def self.genre_count
+    @@genres
+  end
+
+  def self.artist_count
+    @@artists
+  end
+
+end
